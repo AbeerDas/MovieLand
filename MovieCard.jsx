42267b499b09ac7217ea4c3264d2d6923cfb4e1d
AbeerDas <67808617+AbeerDas@users.noreply.github.com>
2023-10-12 15:47:51 -0400

    Update MovieCard.jsx

--- MovieCard.jsx
+++ MovieCard.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const MovieCard = ({ movie }) => {
+    const [plot, setPlot] = useState('');
+
+    useEffect(() => {
+        // Fetch the plot information from the OMDB API
+        async function fetchPlot() {
+            const response = await fetch(`http://www.omdbapi.com?apikey=2df47785&i=${movie.imdbID}`);
+            const data = await response.json();
+            setPlot(data.Plot);
+        }
+
+        fetchPlot();
+    }, [movie.imdbID]);
+
     return (
         <div className='movie'>
-        <div>
-            <p>{movie.Year}</p>
-        </div>
-        <div>
-            <img src={movie.Poster !== 'N/A' ? movie.Poster: 'https://via.placeholder.com/400'} alt={movie.Title}/>
-        </div>
-        <div>
-            <span>{movie.Type}</span>
-            <h3>{movie.Title}</h3>
-        </div>
+            <div>
+                <p>{movie.Year}</p>
+                <p className='plot'>{plot}</p>
+            </div>
+            <div>
+                <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title} />
+            </div>
+            <div>
+                <span>{movie.Type}</span>
+                <h3>{movie.Title}</h3>
+            </div>
         </div>
     );
 }
